99f2802ba21a1491e4a88952792ff397ae45e401
dimab <dimabru@gmail.com>
2018-06-08 18:11:15 +0300

    Create service project

---
+++ BackupService/BackupService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace BackupService
+{
+    static class Program
+    {
+        private static RegistryKey key;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ BackupService/BackupService/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private EventLog eventLog;
+        private Component component;
+
+        public Service1()
+        {
+            InitializeComponent();
+            eventLog = new EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog.Source = "MySource";
+            eventLog.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
